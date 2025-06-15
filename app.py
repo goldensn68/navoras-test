d2523611d75fa6f37e8a59e048cb7e051989a9e4
Søren Nielsen <srn.broager@gmail.com>
2025-06-15 22:47:00 +0200

    Upload af Navoras v0.1 – dummydata og fungerende system

--- app.py
+++ app.py
@@ -1,12 +1,13 @@
+
 from flask import Flask, render_template
 import os
 
 app = Flask(__name__)
 
-@app.route('/')
-def index():
-    return render_template("index.html")
+@app.route("/")
+def home():
+    return render_template("dashboard.html")
 
-if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
