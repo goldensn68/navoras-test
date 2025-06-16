35cd2ad2adba587bc4737669f06dbc8f18f6a8a5
Søren Nielsen <srn.broager@gmail.com>
2025-06-16 08:56:53 +0200

    Opdateret testversion v0.1 med login og tjekliste

--- app.py
+++ app.py
@@ -1,13 +1,10 @@
-
+# Flask entry point
 from flask import Flask
-import os
-
 app = Flask(__name__)
 
-@app.route("/")
+@app.route('/')
 def home():
-    return "Navoras v0.1 kører på Render!"
+    return 'Navoras v0.1 kører på Render!'
 
-if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=10000)
