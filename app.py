9d25d8df52be544a882e6a8a3fb6b490175a9cfc
Søren Nielsen <srn.broager@gmail.com>
2025-06-17 13:33:59 +0200

    Tilføjet testklar version v0.1 med login og dashboard

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, redirect, request, session, url_for
+import os
 
 app = Flask(__name__)
 app.secret_key = 'hemmelig-nøgle'
@@ -31,4 +32,5 @@ def logout():
     return redirect(url_for('login'))
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
