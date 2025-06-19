7197426fb8cd5e1e044441f8607e09b8cfff2723
Søren Nielsen <srn.broager@gmail.com>
2025-06-19 12:11:06 +0200

    Rettet KeyError: session['role']

--- app.py
+++ app.py
@@ -1,15 +1,2 @@
-
-from flask import Flask
-
-app = Flask(__name__)
-
-@app.route('/')
-def index():
-    return 'Navoras v0.1 Testside'
-
-@app.route('/test')
-def test():
-    return 'Testside kører korrekt.'
-
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=10000)
+# Navoras Test App - v0.1 Test6
+print('App running')
